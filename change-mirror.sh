297ad4d72697464d3b7dd35c166efc684172b2dd
Tony <1462668901@qq.com>
2020-07-26 11:17:58 +0800

    Create change-mirror.sh

---
+++ change-mirror.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+echo "备份原有apt源..."
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+sudo cp /etc/apt/sources.list.d/raspi.list /etc/apt/sources.list.d/raspi.list.bak
+
+echo "更换软件源..."
+echo "" > /etc/apt/sources.list
+echo "" > /etc/apt/sources.list.d/raspi.list
+
+cat >> /etc/apt/sources.list <<EOF
+deb http://mirrors.tuna.tsinghua.edu.cn/raspberry-pi-os/raspbian/ buster main non-free contrib rpi
+deb-src http://mirrors.tuna.tsinghua.edu.cn/raspberry-pi-os/raspbian/ buster main non-free contrib rpi
+EOF
+
+sudo apt-get update
+sudo apt-get upgrade
+
+cat >> /etc/apt/sources.list.d/raspi.list <<EOF
+deb http://mirrors.tuna.tsinghua.edu.cn/raspberrypi/ buster main ui
+EOF
+echo "更换apt源完成"
+
+echo "更换pip源..."
+pip install pip -U
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+echo "更换pip源完成"
+
+echo "更换docker源..."
+cat >> /etc/docker/daemon.json <<EOF
+{
+  "registry-mirrors": ["https://registry.docker-cn.com"]
+}
+EOF
+echo "更换docker源完成"
+
+echo "更换npm源..."
+npm config set registry https://registry.npm.taobao.org
+npm config get registry
+echo "更换npm源完成"
